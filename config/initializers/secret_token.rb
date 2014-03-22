770b63ceafea89ff189ba9792e5436d294d90c90
Mikael Hallgren <mikaelhallgren@Mikaels-MacBook-Pro.local>
2014-03-22 18:14:51 +0100

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instalurk::Application.config.secret_key_base = '39a03dc17a8a7d39b5e0d24ed1972917b9fc0470da6d35a425b908cd12b646cd7d557d0d5bed9fd576dd2b4e0bc319baceef87c556c5021f0c3bc93e5eb6e6d9'
